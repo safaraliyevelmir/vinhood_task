b678b76566cb0ff985e46c8a4174999918a38a4f
safaraliyevelmir <safaraliyevelmir@gmail.com>
2023-03-30 17:41:54 +0100

    hello

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class UserModelTest(TestCase):
+    def setUp(self):
+        self.user_email = "example@example.com"
+        self.user_password = "password123"
+        self.user = get_user_model().objects.create_user(
+            email=self.user_email,
+            password=self.user_password
+        )
+
+    def test_user_creation(self):
+        self.assertTrue(isinstance(self.user, get_user_model()))
+        self.assertEqual(self.user.__str__(), self.user.email)
+        self.assertTrue(self.user.is_active)
+        self.assertFalse(self.user.is_staff)
+        self.assertEqual(self.user.date_joined.date(), timezone.now().date())
+        self.assertEqual(self.user.email, self.user_email)
+
+    def test_user_model_manager(self):
+        self.assertEqual(get_user_model().objects.count(), 1)
+        self.assertEqual(get_user_model().objects.create_user(
+            email='example2@example.com',
+            password=self.user_password
+        ).email, 'example2@example.com')
+
+
--- products/tests.py
+++ products/tests.py
@@ -24,3 +24,31 @@ class WishlistItemTestCase(TestCase):
         # Test adding a second product from the same category
         with self.assertRaises(ValidationError):
             WishlistItem.objects.create(user=self.user, product=self.product2)
+
+from django.test import TestCase
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductSerializerTestCase(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user('testuser', password='testpass')
+        self.category = ProductCategory.objects.create(name='TestCategory')
+        self.product = Product.objects.create(name='Test Product 1', price='10.00', rank=4.5, user=self.user, category=self.category)
+        self.serializer = ProductSerializer(instance=self.product)
+
+    def test_contains_expected_fields(self):
+        data = self.serializer.data
+        self.assertCountEqual(data.keys(), ['pk', 'name', 'user', 'price', 'category', 'created_time'])
+
+    def test_name_field_content(self):
+        data = self.serializer.data
+        self.assertEqual(data['name'], self.product.name)
+
+    def test_price_field_content(self):
+        data = self.serializer.data
+        self.assertEqual(data['price'], self.product.price)
+
+
+    def test_read_only_fields(self):
+        self.assertTrue(self.serializer.fields['created_time'].read_only)
+
