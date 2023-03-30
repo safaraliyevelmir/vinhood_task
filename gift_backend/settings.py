7826793e8f2bde9ee9984ea7422f5781ace0dfad
safaraliyevelmir <safaraliyevelmir@gmail.com>
2023-03-30 08:39:26 +0100

    .env

--- gift_backend/settings.py
+++ gift_backend/settings.py
@@ -115,7 +115,11 @@ SIMPLE_JWT = {
 WSGI_APPLICATION = 'gift_backend.wsgi.application'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config("EMAIL_HOST_USER",cast=str)
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD",cast=str)
 
 
 # Database
